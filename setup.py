34cb2a753c2ca9884b6df944fd71106237e9c510
Petr Panov <pvpanov93@gmail.com>
2023-09-17 14:13:10 +0200

    ignore venv

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='enet_plus',
-    version='0.1',
-    packages=['enet_plus'],
-    url='https://github.com/pvpanov/enet_plus',
-    license='MIT',
-    author='ppv',
-    author_email='pvpanov93@gmail.com',
-    description='Elastic net that works simlilar to `sklearn.linear_model.ElasticNet` with - the ability to bound the learned coefficients; - finer control over what coefficients get penalized; - the ability to add extra penalties The package essentially runs `scipy.optimize(method=\'lbfgs\', ...)` in the background.'
+    name="enet_plus",
+    version="0.1",
+    packages=["enet_plus"],
+    url="https://github.com/pvpanov/enet_plus",
+    license="MIT",
+    author="ppv",
+    author_email="pvpanov93@gmail.com",
+    description="Elastic net that works simlilar to `sklearn.linear_model.ElasticNet` with\n"
+    "- the ability to bound the learned coefficients;\n"
+    "- finer control over what coefficients get penalized;\n"
+    " - the ability to add extra penalties.\n"
+    "The package essentially runs `scipy.optimize(method='lbfgs', ...)` in the background.",
 )
